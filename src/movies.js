29e06899d2c2fefd232f19bb489811169bbb9c0c
kings <kingsleyinsaidoo@yahoo.com>
2021-03-25 23:34:42 +0100

    finished first five

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,67 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+let getAllDirectors = (moviesArray) => {
+    // moviesArray => all the info from movies
+    let directorsArr = moviesArray.map(eachMovie => {
+        return eachMovie.director
+
+    })
+    return directorsArr;
+};
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+let howManyMovies = (moviesArray) => {
+    
+    let dramaSpielberg = moviesArray.filter((elem) => {
+        return elem.director === "Steven Spielberg" && elem.genre.includes("Drama") ;
+    })
+    return dramaSpielberg.length
+};
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+let ratesAverage = (moviesArray) => {
+ let ratesTotal = moviesArray.reduce((sum, elem) => {
+     if("rate" in elem) {
+    sum += +(elem.rate/moviesArray.length). toFixed(2)
+     }
+     return sum;
+    }, 0) ;
+    
+    return ratesTotal
+};
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+let dramaMoviesRate = (moviesArray) => {
+    let allDrama = moviesArray.filter((elem, index) => {
+        return elem.genre.includes("Drama")
+    });
+    return ratesAverage(allDrama)
+};
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+let orderByYear = ( moviesArray) =>{
+    let yearSortedMovies = JSON.parse(JSON.stringify(moviesArray));
+
+    yearSortedMovies.sort (( movie1, movie2) => {
+        if (movie1.year > movie2.year ){
+            return 1;
+       }else if(movie1.year < movie2.year) {
+           return -1;
+       
+    }else{
+            if(movie1.title> movie2.title){
+                return 1;
+            }else if (movie1.title < movie2.title) {
+                return -1
+
+            }else{
+                return 0;
+            }
+        }
+    });
+    return yearSortedMovies
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
